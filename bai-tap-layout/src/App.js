442199515f5de89fca9f2610b7500e01acc698c8
BAO TRANG <baogtrang@gmail.com>
2023-09-02 22:52:56 -0500

    updated

---
+++ bai-tap-layout/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Body from './ExLayout/Body';
+import Footer from './ExLayout/Footer';
+import Header from './ExLayout/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ bai-tap-layout/src/ExLayout/Body.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import Banner from "./Banner.js"
+import Items from './Items.js'
+
+export default class Body extends Component {
+  render() {
+    return (
+      <div>Body</div>
+    )
+  }
+}
